dfb3dec77a4f16e900ff5ff054566060d5c52054
Akshay234 <akshayks@thoughtworks.com>
2016-02-06 10:40:44 +0530

    Car movement is possible now

--- javascript/app.js
+++ javascript/app.js
@@ -1,25 +1,21 @@
-var ipc = require("electron").ipcRenderer;
-console.log($(this).closest('img'));
+// var ipc = require("electron").ipcRenderer;
+
+var carDiv = ['l1', 'l2', 'r1', 'r2'];
+
 $(document).keypress(function(e) {
-  console.log(e.which);
+  var id = $('#car').parent().attr('class');
   if(e.which == 65|| e.which == 97){
-    ipc.send('left-key');
-      alert('You pressed left!');
-  }
-  if(e.which == 87|| e.which == 119){
-    ipc.send('up-key');
-      alert('You pressed up!');
+    var car = $('#car')[0];
+    if(carDiv[carDiv.indexOf(id)-1] != undefined){
+      $('.'+carDiv[carDiv.indexOf(id)-1]).append(car);
+    }
   }
   if(e.which == 68|| e.which == 100){
-    ipc.send('right-key');
-      alert('You pressed right!');
-  }
-  if(e.which == 83|| e.which == 115){
-    ipc.send('down-key');
-      alert('You pressed down!');
+    var car = $('#car')[0];
+    console.log(carDiv[carDiv.indexOf(id)+1],carDiv.indexOf(id))
+    if(carDiv[carDiv.indexOf(id)+1] != undefined){
+      $('.'+carDiv[carDiv.indexOf(id)+1]).append(car);
+    }
+
   }
 });
-
-// closeEl.addEventListener('click', function () {
-    // ipc.send('close-main-window');
-// });
